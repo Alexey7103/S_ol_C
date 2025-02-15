9948a6bfc92307c6d3eabe842e05cf2efa73b87f
Alexey7103 <71038@bk.ru>
2025-02-15 17:13:23 +0300

    Применил_арефметику_(#C7.1_1)

--- BaCo/7.1/7.1_1.c
+++ BaCo/7.1/7.1_1.c
@@ -1,1 +1,15 @@
 // Напеши код с арефметическими действиями + - *
+
+#include<stdio.h>
+
+int main(){
+	const int A = 5;
+	const int B = 3;
+	const int C = 1;
+	const int D = 2;
+	const int R = A + B - C * D;
+	printf("Итого: %d" ,R);
+	return 0;
+}
+
+
