31e0d2ddc9fb7e245f976802f154bb02b1a9f227
Konstantin8474 <847454@bk.ru>
2025-02-21 10:19:13 +0300

    Примени_switch_(#C4.1_1)

---
+++ Yc/4.1/4.1_1.c
@@ -0,0 +1,21 @@
+// Перепеши код используч switch
+
+#include<stdio.h>
+
+int main() {
+	int A = 10;
+	if (A == 12) {
+		printf("A меньше");
+	}
+	else if (A == 9) {
+		printf("A больше");
+	}
+	else if (A == 10) {
+		printf("A равно");
+	}
+	else {
+		printf("Не понятно");
+	}
+}
+
+
