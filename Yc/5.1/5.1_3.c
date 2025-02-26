a4164dd4e49ca59f757e1bfa18af391f492ef6ce
Konstantin8474 <847454@bk.ru>
2025-02-26 11:30:12 +0300

    Используй_||_(#C5.1_3)

---
+++ Yc/5.1/5.1_3.c
@@ -0,0 +1,16 @@
+// Усложни.
+
+#include<stdio.h>
+
+int main() {
+	int A = 10;
+	if (A > 5 || A < 1) {
+		printf("%s" ,"Значение больше");
+	}
+	else {
+		printf("%s" ,"Значение не верно");
+	}
+	return 0;
+}
+
+
